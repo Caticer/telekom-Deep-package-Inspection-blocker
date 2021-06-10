a281b84caaf8869de3b918491d5159b99b8d18ce
İbrahim YÖNDEMLİ <doopthe@gmail.com>
2021-06-10 18:33:12 +0300

    more blocked ip

--- start - alternate.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-echo Turk telekom Deep package Inspection blocker
-.\goodbyedpi.exe -2 -p -r -s -f 2 -k 2 -n -e 40 --dns-addr 1.1.1.1 --wrong-chksum --set-ttl 5 & exit
-exit
--- start.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-echo Turk telekom Deep package Inspection blocker
-.\goodbyedpi.exe -4 -p -r -s --dns-addr 1.1.1.1 --wrong-chksum --set-ttl 5 --blacklist tr-blacklist.txt
